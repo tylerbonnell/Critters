29a93368c4c3ee4b54557d62e61371dff88cfb25
Tyler Bonnell <tbonnell@uw.edu>
2016-06-01 20:00:00 -0700

    added enums

--- main.js
+++ main.js
@@ -22,11 +22,65 @@ window.onload = function() {
   setGameLoop(1000);
 }
 
+var Direction = {
+  NORTH: 1,
+  SOUTH: 2,
+  EAST: 3,
+  WEST: 4,
+};
+
+var Action = {
+  HOP: 1,
+  LEFT: 2,
+  RIGHT: 3,
+  INFECT: 4,
+};
+
+var Neighbor = {
+  WALL: 1,
+  EMPTY: 2,
+  SAME: 3,
+  OTHER: 4,
+};
 
 function Bear() {
-  this.char = 'x';
+  this.toString = function() {
+    return 'x';
+  }
+  this.getMove = function(info) {
+
+  }
 }
 
+function CritterInfo() {
+  this.getFront() = function() {
+
+  }
+  this.getBack() = function() {
+
+  }
+  this.getLeft() = function() {
+
+  }
+  this.getRight() = function() {
+
+  }
+  this.getDirection() = function() {
+
+  }
+  this.getFrontDirection() = function() {
+
+  }
+  this.getBackDirection() = function() {
+
+  }
+  this.getLeftDirection() = function() {
+
+  }
+  this.getRightDirection() = function() {
+
+  }
+}
 
 function setGameLoop(rate) {
   if (interval)
@@ -37,6 +91,9 @@ function setGameLoop(rate) {
 }
 
 function addCritter(type, amount) {
+  o = new type();
+  if (!o.hasOwnProperty('toString') || !o.hasOwnProperty('getMove'))
+      return false;
   for (var i = 0; i < amount; i++) {
     if (availableSpots.length > 0) {
       var pos = availableSpots.pop();
@@ -57,7 +114,7 @@ function toString() {
   var str = "";
   for (var y = 0; y < height; y++) {
     for (var x = 0; x < width; x++) {
-      str += grid[x][y] ? grid[x][y].char : " ";
+      str += grid[x][y] ? grid[x][y].toString() : " ";
     }
     str += "\n"
   }
